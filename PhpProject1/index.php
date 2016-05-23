f51f4a99451ea8dcb43e8ec61e3951632e8f3c29
Nichlas <Nichlas@Nichlas-PC.ucn.moc>
2016-05-23 13:25:01 +0200

    Can consume WCF. nom nom nom

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -11,24 +11,24 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        ini_set("soap.wsdl_cache_enabled", "0");
-        $params = array("soap_version" => SOAP_1_2,
-            "trace" => 1,
-            "exceptions" => 0,
+        $url = 'http://localhost:8033/Design_Time_Addresses/Service/BSIService/?wsdl';
+        $params = array(
+            'trace' => 1,
+            'cache_wsdl' => 0,
+            'soap_version' => SOAP_1_1
         );
 
-        $client = new SoapClient('http://localhost:8733/Design_Time_Addresses/Service/Service1/?wsdl', $params);
-        $actionHeader = new SoapHeader('http://www.w3.org/2005/08/addressing', 'Action', 'http://tempuri.org/IService1/GetData', true);
-
-        $client->__setSoapHeaders($actionHeader);
-        
         try {
-            var_dump($client->__getFunctions());
+            $client = new SoapClient($url, $params);
+            $client->__setLocation("http://localhost:8033/Design_Time_Addresses/Service/BSIService");
+
+            //var_dump($client->__getFunctions());
+            $parameters = array('value' => 3);
+            $result = $client->GetData($parameters);
+            echo "Return values: {$result->GetDataResult} <br>";
         } catch (Exception $e) {
             echo 'Caught exception: ', $e->getMessage(), "\n";
         }
-        
-        $client->
         ?>
     </body>
 </html>
